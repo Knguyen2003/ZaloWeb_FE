dd085fa2b9a23503d17dff7de6a4ef2f8eebafb6
Knguyen2003 <nguyenphan1122k3@gmail.com>
2025-04-22 11:34:28 +0700

    update FE

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./services/api/axios.interceptor";
 import "./index.css";
 import { UserProvider } from "./context/UserContext.jsx";
 
